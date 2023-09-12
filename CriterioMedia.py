6143e52476669964ec27a6e04ce156c7453c224b
R. Destro <destro@fei.edu.br>
2023-09-12 20:13:40 -0300

    Update CriterioMedia.py

--- CriterioMedia.py
+++ CriterioMedia.py
@@ -1,3 +1,6 @@
+#pip install scikit-learn==1.0.2
+#pip install matplotlib
+
 import pandas as pd
 import numpy as np
 from sklearn import tree, metrics
@@ -5,26 +8,26 @@ from sklearn.tree import DecisionTreeClassifier
 import matplotlib.pyplot as plt
 from scipy.io import arff
 
-def criterioMedia():
-    data,meta = arff.loadarff('./CriterioMedia.arff')
 
-    attributes = meta.names()
-    data_value = np.asarray(data)
+data,meta = arff.loadarff('./CriterioMedia.arff')
 
-
-    percFalta = np.asarray(data['PercFalta']).reshape(-1,1)
-    notas = np.asarray(data['Nota']).reshape(-1,1)
-    features = np.concatenate((notas , percFalta),axis=1)
-    target = data['resultado']
+attributes = meta.names()
+data_value = np.asarray(data)
 
 
-    Arvore = DecisionTreeClassifier(criterion='entropy').fit(features, target)
+percFalta = np.asarray(data['PercFalta']).reshape(-1,1)
+notas = np.asarray(data['Nota']).reshape(-1,1)
+features = np.concatenate((notas , percFalta),axis=1)
+target = data['resultado']
 
-    plt.figure(figsize=(10, 6.5))
-    tree.plot_tree(Arvore,feature_names=['Notas','PercFalta'],class_names=['Aprovado', 'Reprovado'],
+
+Arvore = DecisionTreeClassifier(criterion='entropy').fit(features, target)
+
+plt.figure(figsize=(10, 6.5))
+tree.plot_tree(Arvore,feature_names=['Notas','PercFalta'],class_names=['Aprovado', 'Reprovado'],
                    filled=True, rounded=True)
-    plt.show()
+plt.show()
 
-    fig, ax = plt.subplots(figsize=(25, 10))
-    metrics.plot_confusion_matrix(Arvore,features,target,display_labels=['Aprovado', 'Reprovado'], values_format='d', ax=ax)
-    plt.show()
+fig, ax = plt.subplots(figsize=(25, 10))
+metrics.plot_confusion_matrix(Arvore,features,target,display_labels=['Aprovado', 'Reprovado'], values_format='d', ax=ax)
+plt.show()
